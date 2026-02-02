9116d33317f122e5fefa64419089faebb87eb1b5
S V Prajwal Kumar <151833010+prajwal7658@users.noreply.github.com>
2026-02-02 20:03:14 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+print("First value:", x)
+print("Second value:", y)
+
+print("Data type of first value:", type(x))
+print("Data type of second value:", type(y))
+
+Sum = int(x) + int(y)
+print("Sum after converting to integers:", Sum)
+
